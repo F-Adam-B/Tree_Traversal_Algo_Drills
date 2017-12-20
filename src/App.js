1da3fc55744e30ea56293e126c805929736c2cae
Nate Garrett <nate.garrett1992@gmail.com>
2017-12-20 11:02:35 -0600

    implemented jsx for linear and binary search

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+      <div>
+      <form className="linear">
+        <p>Linear Search:
         </p>
-      </div>
-    );
+        <input type="input"></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+
+    <div>
+      <form className="binary">
+        <p>Binary Search:
+        </p>
+        <input type="input"></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+  );
   }
 }
 
